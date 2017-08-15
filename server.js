e47f1cdb8f01e36928a8499db3f9a55158309b94
Stephen Schools <stephen.schools>
2017-08-15 14:34:53 -0400

    data showing up, need to style

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const express = require("express");
 const app = express();
+
+const mustacheExpress = require("mustache-express");
+
+const data = require("./data");
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+
+app.get("/", function (request, respond) {
+  respond.send(data);
+});
+
+app.get("/index", function (request, respond) {
+  respond.render("index", {users:data.users});
+});
+
+app.listen(3000, function () {
+  console.log("Successfully started express application.");
+});
